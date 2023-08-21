64b6516f749f50e9a0fa2f5dc285c67c0323ef8d
Rasmus <rasmus.adolfsson99@live.se>
2023-08-21 14:35:33 +0200

    Initial commit

---
+++ src/main/java/com/example/itsakerhetexercise1/Itsakerhetexercise1Application.java
@@ -0,0 +1,13 @@
+package com.example.itsakerhetexercise1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Itsakerhetexercise1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Itsakerhetexercise1Application.class, args);
+    }
+
+}
