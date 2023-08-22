bbae85fc20d46fbb1f71e8b0a8e44fb92da2b84e
JackElvinsson <jackelvinsson@gmail.com>
2023-08-22 11:19:42 +0200

    added AUTO_INCREMENT to user table

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,9 +1,6 @@
+
 create table user (
-    id bigint not null,
-    password varchar(255) not null,
-    username varchar(255) not null,
-    primary key (id)
-);
-create table user_seq (
-    next_val bigint
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
 );
