e1866adf73a75347661a256460cebd9b34aeeb15
Emma Vainio <emmavainio@live.se>
2023-08-25 12:08:39 +0200

    löst lägga till användare

--- src/main/java/com/example/itsakerhetexercise1/controllers/AuthenticationController.java
+++ src/main/java/com/example/itsakerhetexercise1/controllers/AuthenticationController.java
@@ -1,12 +1,12 @@
 package com.example.itsakerhetexercise1.controllers;
 
 import com.example.itsakerhetexercise1.models.RegistrationDTO;
-import com.example.itsakerhetexercise1.models.User;
 import com.example.itsakerhetexercise1.services.AuthenticationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 @RequestMapping("/auth")
 @CrossOrigin("*")
 public class AuthenticationController {
